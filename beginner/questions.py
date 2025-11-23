9288d58183ae9e1783eb0d54acff474e0a352676
abiola814 <oladunjoye1998@gmail.com>
2025-11-23 21:18:20 +0000

    question added

---
+++ beginner/questions.py
@@ -0,0 +1,8 @@
+"""
+difference btw list and tuple
+a list is mutable that which mean it can be changed when create while a tuple does not chnage when create
+how input work
+using keyword called input
+e.g input("enter your question")
+
+"""
